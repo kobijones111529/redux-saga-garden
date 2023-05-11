65b9596cc4c2c8daa38da4d4a38f8591cbb966f4
Kobi Jones <kobijonesmail@gmail.com>
2023-05-10 22:39:43 -0500

    POST new plant using saga

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { legacy_createStore as createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import createSagaMiddleware from 'redux-saga'
-import { put, takeLatest } from 'redux-saga/effects'
+import { put, takeEvery, takeLatest } from 'redux-saga/effects'
 
 import App from './App';
 import logger from 'redux-logger';
@@ -20,8 +20,6 @@ const plantList = (state = [], action) => {
   switch (action.type) {
     case 'SET_PLANTS':
       return action.payload;
-    case 'ADD_PLANT':
-      return [ ...state, action.payload ]
     default:
       return state;
   }
@@ -32,8 +30,14 @@ function* fetchPlants() {
   yield put({ type: 'SET_PLANTS', payload: response.data });
 }
 
+function* addPlant(action) {
+  yield axios.post('/api/plant', { name: action.payload });
+  yield put({ type: 'FETCH_PLANTS' });
+}
+
 function* rootSaga() {
   yield takeLatest('FETCH_PLANTS', fetchPlants);
+  yield takeEvery('ADD_PLANT', addPlant);
 }
 
 const sagaMiddleware = createSagaMiddleware();
